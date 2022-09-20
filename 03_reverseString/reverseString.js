0232029856bf1fe4ca923947b1c964fcba3c0a99
Wergugy <wergugy@gmail.com>
2022-09-20 01:54:25 -0700

    Meh this one...

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    //const letters = [string.split('')];
+    //return letters.reverse().join('');
+    //let rString = rLetters.join("");
+    //return rString;
+    return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
