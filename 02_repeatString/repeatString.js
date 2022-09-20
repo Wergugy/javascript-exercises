b5c3ca1793a8d651674082af6096394478100245
Wergugy <wergugy@gmail.com>
2022-09-20 00:55:46 -0700

    Solution for 02
    passes all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let finString = "";
+    if(num < 0 ) return 'ERROR'
+    while (num >= 1) {
+        finString = finString + string;
+        --num;
+    }
+    return finString;
 };
 
 // Do not edit below this line
