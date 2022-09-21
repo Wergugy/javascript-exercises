a8469f90a5b5faa9d2e721f5618b67f29ae88698
Wergugy <wergugy@gmail.com>
2022-09-20 23:06:44 -0700

    Add solution for 05
    This one was a little easier but only cause
    the last one had arrays which were new.
    learned something new so that always means
    the lesson was worth it!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
 
-};
+    if ((Number.isInteger(a)) && (Number.isInteger(b)) && (a + b) && (a > 0) && (b > 0)) {
+        if (a < b) {
+            let sum = a - 1;
+            for (a; a <= b; ++a) {
+                sum += a;
+            }
+            return sum;
+        }
+        else if (b < a) {
+            let sum = b - 1;
+            for (b; b <= a; ++b) {
+                sum += b;
+            }
+            return sum;
+        }
+    } 
+    else {return 'ERROR'}
+}
 
 // Do not edit below this line
 module.exports = sumAll;
