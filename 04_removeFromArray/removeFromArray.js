15613feb0590fb18ebc181e2fd21235360f69f56
Wergugy <wergugy@gmail.com>
2022-09-20 22:12:06 -0700

    Add solution for 04
    This one was tough almost gave in but
    finally got it. Was so close the whole
    time glad i pulled through!

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...cuts) {
+    const passed = array.filter((value) => {
+        for (const cut of cuts) {
+            if (value === cut) return (1 === 0);
+            else continue;
+        }
+        return value;
+    });
+    return passed;
 };
 
 // Do not edit below this line
