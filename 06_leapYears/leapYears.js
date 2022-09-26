c741f587178a07d716968dd939363127e733f802
Wergugy <wergugy@gmail.com>
2022-09-25 18:22:44 -0700

    Add solution for 06
    One more to go! I learned something about
    switch statements vrs if else statements,
    think i know when to use each a little
    better. Gonna experiment further in the
    future.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,8 +1,14 @@
 const leapYears = function(a) {
-    if ((a % 4) === 0 && 
-    
-    (((a % 100) === 0) && ((a % 400) === 0) && 0 )
+    if ((a % 4) === 0) {
+        if ((a % 100) === 0) {
+            if ((a % 400) === 0) return true;
+            else return false;
+        }
+        else return true;
+    }
+    else return false;
 };
-
+//((700 % 400) != 0)
 // Do not edit below this line
 module.exports = leapYears;
+
