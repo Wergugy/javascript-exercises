b3af222f0cf1d1175b3d973584fb7267bcaaa2b6
Wergugy <wergugy@gmail.com>
2022-09-29 00:54:10 -0700

    Solution for 08.
    Was fairly simple?

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 0) return 1;
+  const factors = [num,];
+  let factor = num -1;
+  while (factor >= 1) {
+    factors.push(factor--);
+  }
+  return factors.reduce((a,b) => a * b);
 };
 
 // Do not edit below this line
