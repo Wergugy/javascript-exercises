b4f8d493214a12c6fd3e3ec4d7e03dc745db1d05
Wergugy <wergugy@gmail.com>
2022-09-30 00:32:07 -0700

    Eww

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
+        const array1 = string.toLowerCase().split('');
+        const filArray = array1.filter((letter) => {
+                if ((letter !== ',') && (letter !== '!') &&
+                 (letter !== '.') && (letter !== ' ')) return true;
+                else return false;
+        });
+        
+        const revArray = filArray.reverse();
+        //return revArray;
+       const result = [];
+        for (let i = 0, l = filArray.length; i < l; i++) {
+               if (filArray[i] === revArray[i]) {
+                result.push(1);
+               }
+               else result.push(0);
+        };
 
+       return result.reduce((a,b) => {
+                const isPal = a + b;
+                return !!(isPal);
+        });
+        
 };
 
 // Do not edit below this line
