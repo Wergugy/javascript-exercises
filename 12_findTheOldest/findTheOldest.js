75b6de8ee867cd8775c367492d8930385e7b0855
Wergugy <wergugy@gmail.com>
2022-09-30 21:22:00 -0700

    Solution for 12
    Just had to refactor some previous code to work here.
    Noice!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+        const thisYear = new Date().getFullYear();
+        people.forEach((person) => {
+                if (!person.yearOfDeath) person.yearOfDeath = thisYear;
+        });
+        const oldest = people.sort((a, b) => {
+                const lastPerson = a.yearOfDeath - a.yearOfBirth;
+                const nextPerson = b.yearOfDeath - b.yearOfBirth;
+                return lastPerson > nextPerson ? -1 : 1;
+              });
+              return oldest[0];
 };
 
 // Do not edit below this line
