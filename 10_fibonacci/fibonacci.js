23923628c55868c0ea61af6b392bc23db8d959f0
Wergugy <wergugy@gmail.com>
2022-09-30 20:27:02 -0700

    Solution for 10
    Spent way too much time on a solution that was
    kind of a kobayashi maru. After finding the formula
    this was a breeze. Next time I'll look to find
    a less complicated way, or maybe try breaking
    it down further.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(iterN) { 
+        if (iterN < 0) return 'OOPS';
+        const golden = 1.618034
+        return Math.floor(((golden ** iterN) - ((1 - golden) ** iterN))/(Math.sqrt(5)));
 };
 
 // Do not edit below this line
